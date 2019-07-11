596249d0fbdab67d06d17442ded1bae46e0ebf26
Mushroms <macbookair@MacbookAirs-MacBook-Air.local>
2019-07-11 14:10:07 +0300

    add firebase

--- android/app/src/main/java/com/organizeme/MainApplication.java
+++ android/app/src/main/java/com/organizeme/MainApplication.java
@@ -3,6 +3,7 @@ package com.organizeme;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RealmReactPackage(),
             new VectorIconsPackage()
       );
